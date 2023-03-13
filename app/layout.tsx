fb74e22e813d9f6bf988b7192f9bce838c0194fe
Sepehr Safari <safari.sepehr@gmail.com>
2023-03-13 13:10:25 +0330

    update metatags

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,19 @@ import "./globals.css";
 export const metadata = {
   title: "Nostribe",
   description: "Join the tribe, join the vibe.",
+  keywords:
+    "Nostribe, Nostr, Nostr protocol, decentralized, censorship-resistant, social media, web client, social network",
+  robots: "index, follow",
+  openGraph: {
+    type: "website",
+    title: "Nostribe",
+    description: "Join the tribe, join the vibe.",
+    images: [
+      {
+        url: "https://raw.githubusercontent.com/sepehr-safari/nostribe-web-client/main/public/nostribe.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
