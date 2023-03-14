d5bf05db2571950b2c1f308c9e5425ae15af7f90
Sepehr Safari <safari.sepehr@gmail.com>
2023-03-15 00:37:01 +0330

    add avatar component

---
+++ components/Avatar/index.tsx
@@ -0,0 +1,17 @@
+export default function Avatar({
+  url,
+  width = 'w-12',
+}: {
+  url: string;
+  width?: 'w-6' | 'w-12' | 'w-24';
+}) {
+  return (
+    <>
+      <div className="avatar">
+        <div className={`mask mask-squircle ${width}`}>
+          <img src={url} />
+        </div>
+      </div>
+    </>
+  );
+}
