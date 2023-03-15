b897ead9373f9dedf48a92e24a2469e410e4af0d
Sepehr Safari <safari.sepehr@gmail.com>
2023-03-16 03:10:30 +0330

    add global feed functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,128 @@
-import Image from 'next/image';
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import { SimplePool } from 'nostr-tools';
 
 import { PostCard } from '@/components';
 
-import logoImage from '../public/nostribe.png';
+import { IPost } from '@/types';
+
+// [TODO] Do you love Spaghetti or what?
 
 export default function Feed() {
+  const [posts, setPosts] = useState<IPost[]>([]);
+
+  useEffect(() => {
+    const pool = new SimplePool();
+
+    const relays = [
+      'wss://relay.damus.io',
+      'wss://relay.snort.social',
+      'wss://eden.nostr.land',
+      'wss://relay.nostr.info',
+      'wss://offchain.pub',
+      'wss://nostr-pub.wellorder.net',
+      'wss://nostr.fmt.wiz.biz',
+      'wss://nos.lol',
+    ];
+
+    const fetchPosts = () => {
+      const subscribeGlobalPosts = pool.sub(relays, [
+        {
+          kinds: [1],
+          limit: 20,
+        },
+      ]);
+
+      subscribeGlobalPosts.on('event', (globalPostsEvent: any) => {
+        const postObj: IPost = {
+          id: globalPostsEvent.id,
+          content: globalPostsEvent.content,
+          author: {
+            pubkey: globalPostsEvent.pubkey,
+          },
+        };
+
+        const subscribeGlobalAuthors = pool.sub(relays, [
+          { authors: [postObj.author.pubkey], kinds: [0] },
+        ]);
+
+        subscribeGlobalAuthors.on('event', (globalAuthorsEvent: any) => {
+          const authorDetail = JSON.parse(globalAuthorsEvent.content);
+
+          postObj.author = {
+            ...postObj.author,
+            name: authorDetail.name,
+            nip05: authorDetail.nip05,
+            profilePicture: authorDetail.picture,
+          };
+
+          setPosts((oldPosts: IPost[]) => [...oldPosts, postObj]);
+        });
+
+        subscribeGlobalAuthors.on('eose', () => {
+          subscribeGlobalAuthors.unsub();
+        });
+
+        const subscribePostReactions = pool.sub(relays, [
+          {
+            kinds: [7],
+            '#e': [postObj.id],
+          },
+        ]);
+
+        subscribePostReactions.on('event', (reactionEvent: any) => {
+          if (reactionEvent.content === '-') {
+            return;
+          }
+
+          const likerPubkey = reactionEvent.pubkey;
+
+          setPosts((oldPosts: IPost[]) =>
+            oldPosts.map((item) => {
+              if (item.id !== postObj.id) {
+                return item;
+              }
+
+              return {
+                ...item,
+                reactions: {
+                  ...item.reactions,
+                  likes: item.reactions?.likes
+                    ? [...item.reactions?.likes, { pubkey: likerPubkey }]
+                    : [{ pubkey: likerPubkey }],
+                },
+              };
+            })
+          );
+        });
+      });
+
+      subscribeGlobalPosts.on('eose', () => {
+        subscribeGlobalPosts.unsub();
+      });
+    };
+
+    fetchPosts();
+
+    return () => {
+      pool.close(relays);
+    };
+  }, []);
+
   return (
     <>
-      <PostCard>
-        <p>Hi From Nostribe &#128075;</p>
-      </PostCard>
-
-      <PostCard>
-        <p>
-          Nostribe is an open-source web client for the decentralized and
-          censorship-resistant Nostr protocol. It provides a social media
-          platform for Nostribians to communicate freely and securely. Its
-          mission is to foster a transparent tribe where everyone has an equal
-          voice and a sense of belonging.
-        </p>
-      </PostCard>
-
-      <PostCard>
-        <Image
-          src={logoImage}
-          alt="Nostribe Logo"
-          width={340}
-          height={340}
-          placeholder="blur"
-          className="rounded-2xl self-center"
-        />
-
-        <p>Just generated our logo with Midjourney &#10024;</p>
-      </PostCard>
+      {posts &&
+        posts.map((post, index: number) => (
+          <PostCard
+            key={index}
+            id={post.id}
+            content={post.content}
+            author={post.author}
+            reactions={post.reactions}
+          />
+        ))}
     </>
   );
 }
--- components/PostCard/index.tsx
+++ components/PostCard/index.tsx
@@ -1,3 +1,5 @@
+import { IPost } from '@/types';
+
 import {
   ArrowPathIcon,
   BoltIcon,
@@ -8,22 +10,33 @@ import {
 
 import Avatar from '../Avatar';
 
-import { PropsWithChildren } from 'react';
+export default function PostCard({ author, content, reactions }: IPost) {
+  const imageRegex =
+    /(?:https?:\/\/)?(?:www\.)?\S+\.(?:jpg|jpeg|png|gif|bmp)/gi;
+  const imageInsideContent = content.match(imageRegex);
 
-export default function PostCard({ children }: PropsWithChildren) {
   return (
     <>
       <article className="card bg-neutral text-neutral-content shadow-2xl shadow-black">
         <div className="card-body p-4">
           <div className="flex flex-col gap-2">
-            <div className="flex gap-2 items-center">
-              <Avatar url="/nostribe.png" width="w-12" />
+            <div className="flex gap-4 items-center">
+              <Avatar
+                url={author.profilePicture || '/nostribe.png'}
+                width="w-12"
+              />
 
               <div className="flex flex-col">
-                <p className="text-lg font-bold">Himmel</p>
-                <p className="text-sm font-bold bg-gradient-to-r from-warning to-accent text-transparent bg-clip-text">
-                  himmel@nostribe.com
-                </p>
+                {author.name && (
+                  <p className="text-lg font-bold">
+                    {author.name.slice(0, 35)}
+                  </p>
+                )}
+                {author.nip05 && (
+                  <p className="text-sm font-bold bg-gradient-to-r from-warning to-accent text-transparent bg-clip-text">
+                    {author.nip05.slice(0, 35)}
+                  </p>
+                )}
               </div>
 
               <div className="ml-auto">
@@ -34,26 +47,37 @@ export default function PostCard({ children }: PropsWithChildren) {
               </div>
             </div>
 
-            <div className="flex flex-col break-words gap-2 ml-14 mr-2">
-              {children}
+            <div className="flex flex-col break-words gap-2 ml-16 mr-2">
+              {imageInsideContent && (
+                <div className="relative object-contain overflow-hidden w-2/3">
+                  <img src={imageInsideContent[0]} />
+                </div>
+              )}
+
+              {content}
             </div>
           </div>
 
-          <div className="card-actions">
-            <button className="btn btn-ghost btn-circle">
+          <div className="card-actions w-full flex justify-evenly">
+            <button className="btn btn-ghost btn-sm px-2 gap-2">
               <BoltIcon width={24} />
+              {}
             </button>
 
-            <button className="btn btn-ghost btn-circle">
+            <button className="btn btn-ghost btn-sm px-2 gap-2">
               <ChatBubbleOvalLeftIcon width={24} />
+              {}
             </button>
 
-            <button className="btn btn-ghost btn-circle">
+            <button className="btn btn-ghost btn-sm px-2 gap-2">
               <HeartIcon width={24} />
+
+              {reactions?.likes && reactions?.likes.length}
             </button>
 
-            <button className="btn btn-ghost btn-circle">
+            <button className="btn btn-ghost btn-sm px-2 gap-2">
               <ArrowPathIcon width={24} />
+              {}
             </button>
           </div>
         </div>
