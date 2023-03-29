a197eca8a65f5641362dfea11666af4e980be15b
Sepehr Safari <safari.sepehr@gmail.com>
2023-03-30 00:41:36 +0330

    add another size for avatars

--- components/Avatar/index.tsx
+++ components/Avatar/index.tsx
@@ -3,7 +3,7 @@ export default function Avatar({
   width = 'w-12',
 }: {
   url: string;
-  width?: 'w-6' | 'w-12' | 'w-24' | 'w-36';
+  width?: 'w-6' | 'w-12' | 'w-14' | 'w-24' | 'w-36';
 }) {
   return (
     <>
