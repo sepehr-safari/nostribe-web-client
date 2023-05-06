95a06b95449c669b3a1d6e98a83d29ef81efa190
Martti Malmi <sirius@iki.fi>
2023-05-06 10:45:09 +0300

    follow button following status

--- components/FollowButton.tsx
+++ components/FollowButton.tsx
@@ -1,7 +1,21 @@
+import useStore from "@/store";
+import {useProfileContacts} from "@/hooks";
+import {toHexKey} from "@/utils/HexKey";
+
 export default function FollowButton({ pub }: { pub: string }) {
+  const userData = useStore((state) => state.auth.user.data);
+  const {
+    contactEvents,
+  } = useProfileContacts(userData?.publicKey || '');
+
+  // TODO probably need faster way to get contact list
+  const event = contactEvents[0];
+  const hexPub = toHexKey(pub);
+  const isFollowing = event?.tags?.some((tag) => tag[0] === "p" && tag[1] === hexPub);
+  console.log('hexPub', hexPub, 'event', event, 'isFollowing', isFollowing);
   return (
     <button className="btn btn-sm gap-2">
-      Follow
+      {isFollowing ? 'Unfollow' : 'Follow'}
     </button>
   )
 }
