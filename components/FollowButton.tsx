7eda1a83c1018c552898b947fb328fb4546865f3
Martti Malmi <sirius@iki.fi>
2023-05-06 11:18:56 +0300

    publish follow event

--- components/FollowButton.tsx
+++ components/FollowButton.tsx
@@ -1,21 +1,46 @@
 import useStore from "@/store";
-import {useProfileContacts} from "@/hooks";
-import {toHexKey} from "@/utils/HexKey";
+import { useProfileContacts } from "@/hooks";
+import { toHexKey } from "@/utils/HexKey";
+import usePublish from "@/hooks/usePublish";
+import { Event } from "nostr-tools";
 
 export default function FollowButton({ pub }: { pub: string }) {
   const userData = useStore((state) => state.auth.user.data);
-  const {
-    contactEvents,
-  } = useProfileContacts(userData?.publicKey || '');
+  const { contactEvents } = useProfileContacts(userData?.publicKey || "");
+  const publish = usePublish();
 
-  // TODO probably need faster way to get contact list
-  const event = contactEvents[0];
+  const event = contactEvents?.reduce((prev, curr) => {
+    if (!prev) return curr;
+    if (curr.created_at > prev.created_at) return curr;
+    return prev;
+  }, null as Event | null);
+
   const hexPub = toHexKey(pub);
   const isFollowing = event?.tags?.some((tag) => tag[0] === "p" && tag[1] === hexPub);
-  console.log('hexPub', hexPub, 'event', event, 'isFollowing', isFollowing);
+  console.log("hexPub", hexPub, "event", event, "isFollowing", isFollowing);
+
+  const onClick = () => {
+    let newTags;
+    if (isFollowing) {
+      newTags = (event?.tags || []).filter((tag) => tag[0] !== "p" || tag[1] !== hexPub);
+    } else {
+      newTags = (event?.tags || []).concat([["p", hexPub]]);
+    }
+    publish({
+      tags: newTags,
+      content: event?.content || '',
+      kind: 3,
+    });
+    console.log('publishing', {
+      tags: newTags,
+      content: event?.content,
+      kind: 3,
+    });
+  };
+
   return (
-    <button className="btn btn-sm gap-2">
-      {isFollowing ? 'Unfollow' : 'Follow'}
+    <button className="btn btn-sm gap-2" onClick={onClick}>
+      {isFollowing ? "Unfollow" : "Follow"}
     </button>
-  )
+  );
 }
