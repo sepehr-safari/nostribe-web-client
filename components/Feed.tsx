af77b34780dc8ba7e09938e1e60410d25b9458ff
Martti Malmi <sirius@iki.fi>
2023-05-11 00:05:29 +0300

    also show vids on grid

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -11,6 +11,12 @@ import Modal from "@/components/modal/Modal";
 const PAGE_SIZE = 6;
 const LOAD_MORE_MARGIN = '0px 0px 2000px 0px';
 
+const VideoIcon = (
+  <svg width="18" viewBox="0 0 122.88 111.34" fill="currentColor">
+    <path d="M23.59,0h75.7a23.68,23.68,0,0,1,23.59,23.59V87.75A23.56,23.56,0,0,1,116,104.41l-.22.2a23.53,23.53,0,0,1-16.44,6.73H23.59a23.53,23.53,0,0,1-16.66-6.93l-.2-.22A23.46,23.46,0,0,1,0,87.75V23.59A23.66,23.66,0,0,1,23.59,0ZM54,47.73,79.25,65.36a3.79,3.79,0,0,1,.14,6.3L54.22,89.05a3.75,3.75,0,0,1-2.4.87A3.79,3.79,0,0,1,48,86.13V50.82h0A3.77,3.77,0,0,1,54,47.73ZM7.35,26.47h14L30.41,7.35H23.59A16.29,16.29,0,0,0,7.35,23.59v2.88ZM37.05,7.35,28,26.47H53.36L62.43,7.38v0Zm32,0L59.92,26.47h24.7L93.7,7.35Zm31.32,0L91.26,26.47h24.27V23.59a16.32,16.32,0,0,0-15.2-16.21Zm15.2,26.68H7.35V87.75A16.21,16.21,0,0,0,12,99.05l.17.16A16.19,16.19,0,0,0,23.59,104h75.7a16.21,16.21,0,0,0,11.3-4.6l.16-.18a16.17,16.17,0,0,0,4.78-11.46V34.06Z" />
+  </svg>
+)
+
 type Props = {
   isEmpty?: boolean;
   events: Event[];
@@ -18,10 +24,15 @@ type Props = {
 
 type DisplayAs = 'feed' | 'grid';
 
+type ImageOrVideo = {
+  type: 'image' | 'video';
+  url: string;
+}
+
 const Feed = ({ isEmpty, events }: Props) => {
   const [displayCount, setDisplayCount] = useState(PAGE_SIZE);
   const [displayAs, setDisplayAs] = useState('feed' as DisplayAs);
-  const [modalImageIndex, setModalImageIndex] = useState(null as number | null);
+  const [modalItemIndex, setModalImageIndex] = useState(null as number | null);
   const lastElementRef = useRef(null);
 
   useEffect(() => {
@@ -54,23 +65,24 @@ const Feed = ({ isEmpty, events }: Props) => {
     };
   }, [events, displayCount, lastElementRef.current]);
 
-  const imageUrls = useMemo(() => events
+  const imagesAndVideos = useMemo(() => events
     .sort((a, b) => b.created_at - a.created_at)
     .flatMap((event) => {
-      const matches = event.content.match(Image.regex);
-      return matches || [];
+      const imageMatches = (event.content.match(Image.regex) || []).map(url => ({ type: 'image', url }));
+      const videoMatches = (event.content.match(Video.regex) || []).map(url => ({ type: 'video', url }));
+      return [...imageMatches, ...videoMatches];
     })
     .slice(0, displayCount), [events, displayCount]);
 
   const goToPrevImage = () => {
-    if (modalImageIndex === null) return;
-    const prevImageIndex = (modalImageIndex - 1 + imageUrls.length) % imageUrls.length;
+    if (modalItemIndex === null) return;
+    const prevImageIndex = (modalItemIndex - 1 + imagesAndVideos.length) % imagesAndVideos.length;
     setModalImageIndex(prevImageIndex);
   };
 
   const goToNextImage = () => {
-    if (modalImageIndex === null) return;
-    const nextImageIndex = (modalImageIndex + 1) % imageUrls.length;
+    if (modalItemIndex === null) return;
+    const nextImageIndex = (modalItemIndex + 1) % imagesAndVideos.length;
     setModalImageIndex(nextImageIndex);
   };
 
@@ -88,7 +100,7 @@ const Feed = ({ isEmpty, events }: Props) => {
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, [modalImageIndex]);
+  }, [modalItemIndex]);
 
   if (isEmpty) return <p>No Posts</p>;
 
@@ -120,31 +132,49 @@ const Feed = ({ isEmpty, events }: Props) => {
   const renderGrid = () => {
     return (
       <div className="grid grid-cols-3 gap-px">
-        {imageUrls.map((imageUrl, index) => renderGridItem(imageUrl, index))}
+        {imagesAndVideos.map((item, index) => renderGridItem(item, index))}
       </div>
     );
   }
 
-  const renderGridItem = (imageUrl: string, index: number) => {
+  const renderGridItem = (item: { url: string, type: "image" | "video" }, index: number) => {
+    const url = (item.type === 'video') ? `https://imgproxy.iris.to/thumbnail/428/${item.url}`
+      : `https://imgproxy.iris.to/insecure/rs:fill:428:428/plain/${item.url}`;
     return (
       <div
-        key={`feed${imageUrl}${index}`}
-        className="aspect-square cursor-pointer"
-        ref={index === imageUrls.length - 1 ? lastElementRef : null}
+        key={`feed${url}${index}`}
+        className="aspect-square cursor-pointer relative"
+        ref={index === imagesAndVideos.length - 1 ? lastElementRef : null}
         onClick={() => {
           setModalImageIndex(index);
         }}
       >
-        <img src={imageUrl} alt="" className="w-full h-full object-cover" />
+        <img src={url} alt="" className="w-full h-full object-cover" />
+        {item.type === 'video' && (
+          <div className="absolute top-0 right-0 m-2 shadow-md shadow-gray-500 ">
+            {VideoIcon}
+          </div>
+        )}
+
       </div>
     );
   }
 
   const renderImageModal = () => {
-    return modalImageIndex !== null ? (
+    return modalItemIndex !== null ? (
       <Modal onClose={() => setModalImageIndex(null)}>
         <div className="relative w-full h-full flex justify-center">
-          <img className="rounded max-h-[90vh] max-w-[90vw] object-contain" src={imageUrls[modalImageIndex]} />
+          {imagesAndVideos[modalItemIndex].type === 'video' ? (
+            <video
+              className="rounded max-h-[90vh] max-w-[90vw] object-contain"
+              src={imagesAndVideos[modalItemIndex].url}
+              controls
+              muted
+              autoPlay
+            />
+          ) : (
+            <img className="rounded max-h-[90vh] max-w-[90vw] object-contain" src={imagesAndVideos[modalItemIndex].url} />
+          )}
           <div className="flex items-center justify-between w-full h-full absolute bottom-0 left-0 right-0">
             <div
               className="p-4"
