8abcf741d7fb8f65dfee6d19f2ac9fb8e8e7a4af
Martti Malmi <sirius@iki.fi>
2023-05-11 11:13:46 +0300

    /note and /npub routes

---
+++ app/(dashboard)/[address]/page.tsx
@@ -0,0 +1,10 @@
+import Post from '@/app/(dashboard)/post/[address]/page';
+import Profile from '@/app/(dashboard)/profile/[address]/page';
+
+export default function Address({params}: {params: {address: string}}) {
+  if (params.address.startsWith('note')) {
+    return <Post params={params} />;
+  } else {
+    return <Profile params={params} />;
+  }
+}
