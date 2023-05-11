8923e1202a873942a257038f88c908a177271ed4
Martti Malmi <sirius@iki.fi>
2023-05-11 18:27:38 +0300

    capitalize header title

--- components/Header.tsx
+++ components/Header.tsx
@@ -63,6 +63,8 @@ const BackNavHeader = () => {
     title = 'Post';
   } else if (pathname === '/profile/edit') {
     title = 'Edit Profile';
+  } else {
+    title = title.charAt(0).toUpperCase() + title.slice(1);
   }
 
   const isMyProfile = pathname.startsWith('/npub') && toHexKey(params.address) === userData?.publicKey;
