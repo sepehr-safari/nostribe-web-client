dff2321e0559a5203d401af2f4ee7126865d921d
Martti Malmi <sirius@iki.fi>
2023-05-16 16:29:46 +0300

    fix

--- app/(dashboard)/notifications/page.tsx
+++ app/(dashboard)/notifications/page.tsx
@@ -2,9 +2,11 @@
 
 import Feed from "@/components/Feed";
 import useStore from "@/store";
-import {useSubscribe} from "nostr-hooks";
+import {Event} from "nostr-tools";
 
 export default function Notifications() {
   const userData = useStore((state) => state.auth.user.data);
-  return <Feed showDisplayAs={false} filters={[{ kinds: [1, 6, 7], limit: 100, '#p': [userData?.publicKey || ''] }]} />;
+  const filters = userData?.publicKey ? [{ kinds: [1, 6, 7], limit: 100, '#p': [userData?.publicKey || ''] }] : [];
+  const filterFn = (event: Event) => event?.pubkey !== userData?.publicKey;
+  return <Feed showDisplayAs={false} filters={filters} filterFn={filterFn} />;
 }
