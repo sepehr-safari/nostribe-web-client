401b536885fddfd103b5d28c42bd076294667322
Martti Malmi <sirius@iki.fi>
2023-05-24 13:12:51 +0300

    only redir if nip05 valid

--- app/(dashboard)/profile/[address]/page.tsx
+++ app/(dashboard)/profile/[address]/page.tsx
@@ -1,34 +1,39 @@
 'use client';
 
-import {memo} from 'react';
+import {memo, useEffect} from 'react';
 
 import {useProfileContent, useProfileHex} from '@/hooks';
 
 import ProfileCard from '@/components/ProfileCard';
 import Feed, {FilterOption} from '@/components/Feed';
-import {useSubscribe} from "nostr-hooks";
-import useStore from "@/store";
-import {Event} from "nostr-tools";
+import {Event, nip05} from "nostr-tools";
 import {getReplyingToEvent} from "@/utils/event";
 
 const Profile = ({ params }: { params: { address: string } }) => {
   const profileHex = useProfileHex(params.address);
   const {
-    nip05,
+    nip05: nip05addr,
   } = useProfileContent(profileHex);
+
+  useEffect(() => {
+    if (nip05addr) {
+      nip05.queryProfile(nip05addr).then((profile) => {
+        if (!profile) {
+          return;
+        }
+        const data = { ...window.history.state };
+        if (nip05addr.endsWith('@iris.to')) {
+          window.history.replaceState(data, '', `/${nip05addr.split('@')[0]}`);
+        }
+        else {
+          window.history.replaceState(data, '', `/${nip05addr}`);
+        }
+      });
+    }
+  });
 
   if (!profileHex) {
     return null;
-  }
-
-  if (nip05) {
-    const data = { ...window.history.state };
-    if (nip05.endsWith('@iris.to')) {
-      window.history.replaceState(data, '', `/${nip05.split('@')[0]}`);
-    }
-    else {
-      window.history.replaceState(data, '', `/${nip05}`);
-    }
   }
 
   const filterOptions: FilterOption[] = [
--- components/Nip05View.tsx
+++ components/Nip05View.tsx
@@ -1,26 +1,41 @@
-function ColoredText({ text }: { text: string }) {
+import { nip05 } from 'nostr-tools';
+import { useEffect, useState } from "react";
+
+function ColoredText({ text, valid }: { text: string, valid: boolean | null }) {
+  const textClass = valid === false ? 'line-through text-neutral-500' : (valid === null ? 'text-neutral-500' : 'text-iris-green');
+  const tooltipClass = valid === false ? 'tooltip tooltip-right' : '';
+  const tooltipText = valid === false ? 'Invalid nip05' : '';
+
   return (
-    <div className="text-iris-green bg-clip-text text-xs font-bold leading-5">
+    <div className={`${textClass} text-left text-xs font-bold leading-5 ${tooltipClass}`} data-tip={tooltipText}>
       {text}
     </div>
   );
 }
 
 export default function Nip05View({ text }: { text: string }) {
+  const [nip05Valid, setNip05Valid] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    nip05.queryProfile(text).then((profile) => {
+      setNip05Valid(!!profile);
+    });
+  });
+
   if (text.startsWith('_@')) {
     text = text.slice(2);
   }
 
   if (text.length < 50) {
-    return <ColoredText text={text} />;
+    return <ColoredText text={text} valid={nip05Valid} />;
   }
 
   const trimmedText = text.slice(0, 25) + '...' + text.slice(-25);
 
   return (
-    <div className="dropdown dropdown-hover">
+    <div className="tooltip">
       <label tabIndex={0}>
-        <ColoredText text={trimmedText} />
+        <ColoredText text={trimmedText} valid={nip05Valid} />
       </label>
       <div
         tabIndex={0}
