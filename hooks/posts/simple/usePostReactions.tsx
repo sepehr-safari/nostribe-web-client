b8ccd11e7e830b32d72c350aca7c7a9affe31410
Sepehr Safari <safari.sepehr@gmail.com>
2023-05-25 14:08:26 +0330

    fix post reactions

--- hooks/posts/simple/usePostReactions.tsx
+++ hooks/posts/simple/usePostReactions.tsx
@@ -1,39 +1,44 @@
 'use client';
 
 import { useSubscribe } from 'nostr-hooks';
+import { useMemo } from 'react';
 
 import useStore from '@/store';
 
 const usePostReactions = (postId: string | undefined) => {
   const relays = useStore((store) => store.relays);
 
-  let { events: reactionEvents, eose: reactionEose } = useSubscribe({
+  const { events, eose } = useSubscribe({
     relays,
     filters: !!postId ? [{ '#e': [postId], kinds: [1, 6, 7, 9735] }] : [],
     options: {
       closeAfterEose: false,
       enabled: !!postId,
-      invalidate: false, // TODO: true causes a refresh loop bug in post reactions
       batchingInterval: 2000,
+      cacheRefresh: true,
     },
   });
 
   // only 1 like or repost per author. TODO kind 1 reposts
-  reactionEvents = reactionEvents.filter((event, index, self) => {
-    if (event.kind === 1 || event.kind === 9735) return true;
-    return (
-      self.findIndex(
-        (e) => e.pubkey === event.pubkey && e.kind === event.kind
-      ) === index
-    );
-  });
+  const reactionEvents = useMemo(
+    () =>
+      events.filter((event, index, self) => {
+        if (event.kind === 1 || event.kind === 9735) return true;
+        return (
+          self.findIndex(
+            (e) => e.pubkey === event.pubkey && e.kind === event.kind
+          ) === index
+        );
+      }),
+    [events]
+  );
 
-  const isFetchingReactions = !reactionEose && !reactionEvents.length;
-  const isReactionsEmpty = reactionEose && !reactionEvents.length;
+  const isFetchingReactions = !eose && !reactionEvents.length;
+  const isReactionsEmpty = eose && !reactionEvents.length;
 
   return {
     reactionEvents,
-    reactionEose,
+    reactionEose: eose,
     isFetchingReactions,
     isReactionsEmpty,
   };
